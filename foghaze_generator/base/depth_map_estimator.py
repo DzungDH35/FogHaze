5d21db28ee15d2a2e71593b8ccbbc7611a7579df
Dzung DH <dzung.dh3510@gmail.com>
2023-05-24 12:20:15 +0700

    Add base code and midas implementation of depth map estimation

---
+++ foghaze_generator/base/depth_map_estimator.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+import cv2 as cv
+import numpy as np
+
+
+class BaseDepthMapEstimator(ABC):
+    _base_images = [] # (np.ndarray[]) - list of base images which are RGB (expected)
+
+
+    # @param (mixture of np.ndarray and str) images
+    def __init__(self, images):
+        self.base_images = images
+        
+
+    # @return np.ndarray[]
+    @property
+    def base_images(self):
+        return self._base_images
+    
+
+    # @param (mixture of np.ndarray and str) images - expect images as numpy array or image file path
+    @base_images.setter
+    def base_images(self, images):
+        if not isinstance(images, list):
+            raise TypeError('Expected a list of images!')
+
+        for i, img in enumerate(images):
+            img_type = type(img)
+            
+            if img_type is str:
+                img = cv.imread(img)
+
+                if img is None:
+                    print('Cannot read an image!')
+                    images.pop(i)
+                else:
+                    images[i] = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+            elif not img_type is np.ndarray:
+                raise TypeError('Not a valid image (numpy array) or string of file path!')
+        
+        self._base_images = images
+
+
+    # @return np.ndarray[]
+    @abstractmethod
+    def estimate_depth_maps(self):
+        pass
---
+++ foghaze_generator/midas_dmap_estimator.py
@@ -0,0 +1,77 @@
+from base.depth_map_estimator import BaseDepthMapEstimator
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+"""
+_midas_model_type = 'DPT_Large'    # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
+_midas_model_type = 'DPT_Hybrid'   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
+_midas_model_type = 'MiDaS_small'  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+"""
+VALID_MIDAS_MODEL_TYPE = ['DPT_Large', 'DPT_Hybrid', 'MiDaS_small']
+
+
+class MidasDmapEstimator(BaseDepthMapEstimator):
+    _midas = None
+    _transform = None
+    _device = None
+
+
+    # @param (mixture of np.ndarray and str) images
+    # @param (str) midas_model_type
+    def __init__(self, images, midas_model_type='DPT_Large'):
+        super().__init__(images)
+
+        if midas_model_type not in VALID_MIDAS_MODEL_TYPE:
+            raise ValueError('Expect midas_model_type to be one of "DPT_Large" or "DPT_Hybrid" or "MiDaS_small"!')
+        
+        self._midas = torch.hub.load('intel-isl/MiDaS', midas_model_type)
+        
+        # Move model to GPU if available
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self._device = device
+        self._midas.to(device)
+        self._midas.eval()
+
+        # Load transforms to resize and normalize the image for large or small model
+        midas_transforms = torch.hub.load('intel-isl/MiDaS', 'transforms')
+        if midas_model_type == 'DPT_Large' or midas_model_type == 'DPT_Hybrid':
+            self._transform = midas_transforms.dpt_transform
+        else:
+            self._transform = midas_transforms.small_transform
+
+
+    # @return np.ndarray[]
+    def estimate_depth_maps(self):
+        depth_maps = []
+
+        if len(self._base_images) == 0:
+            print('No base images to estimate from!')
+            return []
+        
+        # Preprocessing - Load image and apply transforms
+        input_batches = []
+        for img in self._base_images:
+            input_batches.append(self._transform(img).to(self._device))
+        
+        # Predict and resize to original resolution
+        predictions = []
+        with torch.no_grad():
+            for i, input_batch in enumerate(input_batches):
+                prediction = self._midas(input_batch)
+
+                prediction = torch.nn.functional.interpolate(
+                    prediction.unsqueeze(1),
+                    size=self._base_images[i].shape[:2],
+                    mode="bicubic",
+                    align_corners=False,
+                ).squeeze()
+
+                predictions.append(prediction)
+
+        for prediction in predictions:
+            depth_maps.append(prediction.cpu().numpy())
+
+        return depth_maps
+    
