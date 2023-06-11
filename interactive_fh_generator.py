Fix bug wrong order of key passing

interactive_fh_generator.py
@@ -1,11 +1,15 @@
-from asm_foghaze_generator import ASMFogHazeGenerator, ATM_LIGHT_OPMODES, SCATTERING_COEF_OPMODES
+from foghaze_generator.asm_foghaze_generator import ASMFogHazeGenerator, ATM_LIGHT_OPMODES, SCATTERING_COEF_OPMODES
 from matplotlib.widgets import TextBox, Button as PltButton
-from midas_dmap_estimator import MidasDmapEstimator
+from foghaze_generator.midas_dmap_estimator import MidasDmapEstimator
 from tkinter import Tk, filedialog, messagebox, Button as TkButton
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import time
+from ast import literal_eval
+import cv2 as cv
+import traceback
+from foghaze_generator.helper import get_perlin_noise
 
 
 class Experiment2:
@@ -117,37 +121,74 @@ class Experiment2:
 
 class InteractiveFHGenerator:
     _fh_generator: ASMFogHazeGenerator
-    _rgb_image: np.ndarray
-    file_path: str
+    generation_result: tuple
 
 
     def __init__(self, fh_generator: ASMFogHazeGenerator):
         self._fh_generator = fh_generator
 
+    
+    def perlin_noise_image(self):
+        clear = self._fh_generator.rgb_images[0]
+        pnoise_config = self._fh_generator.pnoise_configs[0]
+        pnoise = get_perlin_noise(clear.shape, pnoise_config, (0, 255))
+
+        return pnoise.astype(np.uint8)
+
 
     def input_rgb_image(self):
         file_path = filedialog.askopenfilename(
-            title='Select Image',
+            title='Select RGB Image',
             filetypes=(('Image File', '*.jpg *.jpeg'), ('All Files', '*.*'))
         )
 
         if file_path:
-            image = plt.imread(file_path)
-            self._rgb_image = image
+            try:
+                image = plt.imread(file_path)
+                self._fh_generator.rgb_images = [image]
+                plt.figure(1, clear=True)
+                plt.title(f'Input Image {image.shape}')
+                plt.axis('off')
+                plt.imshow(image)
+                plt.show()
+            except Exception as e:
+                traceback.print_exc()
+                messagebox.showerror('Error', str(e))
 
-            plt.figure(1, clear=True)
-            plt.imshow(image)
-            plt.axis('off')
-            plt.title(f'Input RGB Image {image.shape}')
-            plt.show()
+
+    def input_inverse_dmap(self):
+        file_path = filedialog.askopenfilename(
+            title='Select Depth Map',
+            filetypes=(('Image File', '*.jpg *.jpeg'), ('All Files', '*.*'))
+        )
+
+        if file_path:
+            try:
+                idmap = cv.imread(file_path, cv.IMREAD_GRAYSCALE)
+                self._fh_generator.inverse_dmaps = [idmap]
+                plt.figure(2, clear=True)
+                plt.title(f'Input Image {idmap.shape}')
+                plt.axis('off')
+                plt.imshow(idmap)
+                plt.show()
+            except Exception as e:
+                traceback.print_exc()
+                messagebox.showerror('Error', str(e))
 
 
     def configure_opmode(self):
         current_opmode = self._fh_generator.operation_mode
-        plt.figure(2, clear=True)
+        print(current_opmode)
+        plt.figure(3, clear=True)
 
-        text_box_A = TextBox(plt.axes([0.25, 0.6, 0.5, 0.05]), 'Atmospheric Light', initial=f'{current_opmode["atm_light"]}')
-        text_box_beta = TextBox(plt.axes([0.25, 0.4, 0.5, 0.05]), 'Scattering Coefficient', initial=f'{current_opmode["scattering_coef"]}')
+        axis_A = plt.axes([0.4, 0.6, 0.3, 0.05])
+        text_box_A = TextBox(axis_A, 'Atmospheric Light', initial=f'{current_opmode["atm_light"]}')
+        axis_A.text(0, 1.3, f'Valid values: {ATM_LIGHT_OPMODES}', color='red')
+
+        axis_beta = plt.axes([0.4, 0.4, 0.3, 0.05])
+        text_box_beta = TextBox(axis_beta, 'Scattering Coefficient', initial=f'{current_opmode["scattering_coef"]}')
+        axis_beta.text(0, 1.3, f'Valid values: {SCATTERING_COEF_OPMODES}', color='red')
+
         submit_button = PltButton(plt.axes([0.4, 0.2, 0.2, 0.1]), 'Submit')
 
         def submit_callback(event):
@@ -155,47 +196,70 @@ class InteractiveFHGenerator:
             new_beta_mode = text_box_beta.text
 
             if new_A_mode not in ATM_LIGHT_OPMODES or new_beta_mode not in SCATTERING_COEF_OPMODES:
-                err_msg = 'Wrong configuration value!'
-                messagebox.showerror('Error', err_msg)
-                raise ValueError(err_msg)
+                raise ValueError('Wrong configuration value!')
             
             self._fh_generator.operation_mode['atm_light'] = new_A_mode
             self._fh_generator.operation_mode['scattering_coef'] = new_beta_mode
-            messagebox.showinfo('Configure Opmode', 'Done')
+            print('Successfully configure operation mode!')
 
         submit_button.on_clicked(submit_callback)
         plt.show()
 
 
     def configure_params(self):
-        plt.figure(3, clear=True)
-        default_pnoise = {
-            'base': 0.0,
-            'lacunarity': 2.0,
-            'octaves': 1,
-            'persistence': 0.5,
-            'repeatx': 1024,
-            'repeaty': 1024,
-            'scale': 1
+        plt.figure(4, clear=True)
+        print(self._fh_generator.atm_lights)
+        print(self._fh_generator.scattering_coefs)
+        print(self._fh_generator.pnoise_configs)
+
+        atm_light = self._fh_generator.atm_lights[0] if self._fh_generator.atm_lights else None
+        beta = self._fh_generator.scattering_coefs[0] if self._fh_generator.scattering_coefs else None
+        pnoise_config = self._fh_generator.pnoise_configs[0] if self._fh_generator.pnoise_configs else {}
+        pnoise_config = {
+            'octaves': pnoise_config.get('octaves', 1),
+            'persistence': pnoise_config.get('persistence', 0.5),
+            'lacunarity': pnoise_config.get('lacunarity', 2.0),
+            'repeatx': pnoise_config.get('repeatx', 1024),
+            'repeaty': pnoise_config.get('repeaty', 1024),
+            'base': pnoise_config.get('base', 0.0),
+            'scale': pnoise_config.get('scale', 1)
         }
 
-        atm_light = None
-        beta = None
-        pnoise_config = default_pnoise
         bottom_offset = 0.3
+        left_offset = 0.25
+        input_width = 0.5
+        input_height = 0.05
 
         tb_pnoise = {}
-        for i, (key, value) in enumerate(pnoise_config.items()):
-            tb_pnoise[key] = TextBox(plt.axes([0.25, bottom_offset, 0.5, 0.05]), key.capitalize(), initial=value)
-            bottom_offset += 0.05
+        for key, value in reversed(pnoise_config.items()):
+            tb_pnoise[key] = TextBox(plt.axes([left_offset, bottom_offset, input_width, input_height]), key.capitalize(), initial=value)
+            bottom_offset += 0.06
         
-        tb_A = TextBox(plt.axes([0.25, bottom_offset + 0.2, 0.5, 0.05]), 'Atmospheric Light', initial=atm_light)
-        tb_beta = TextBox(plt.axes([0.25, bottom_offset + 0.1, 0.5, 0.05]), 'Scattering Coefficient', initial=beta)
+        axis_beta = plt.axes([left_offset, bottom_offset + 0.03, input_width, input_height])
+        tb_beta = TextBox(axis_beta, 'Scattering Coefficient', initial=beta)
+        axis_beta.text(0, 1.2, f'Valid type: float | tuple[float, float]', color='red')
+
+        axis_A = plt.axes([left_offset, bottom_offset + 0.15, input_width, input_height])
+        tb_A = TextBox(axis_A, 'Atmospheric Light', initial=atm_light)
+        axis_A.text(0, 1.2, f'Valid type: int | tuple[int, int]', color='red')
 
         submit_button = PltButton(plt.axes([0.4, 0.1, 0.2, 0.1]), 'Submit')
 
         def submit_callback(event):
-            messagebox.showinfo('Configure Opmode', 'Done')
+            new_A = literal_eval(tb_A.text) if tb_A.text else None
+            new_A = None if new_A == -1 else new_A
+            self._fh_generator.atm_lights = [new_A]
+
+            new_beta = literal_eval(tb_beta.text) if tb_beta.text else None
+            new_A = None if new_beta == -1.0 else new_beta
+            self._fh_generator.scattering_coefs = [new_beta]
+
+            new_pnoise_config = {}
+            for key, textbox in tb_pnoise.items():
+                new_pnoise_config[key] = literal_eval(textbox.text)
+            self._fh_generator.pnoise_configs = [new_pnoise_config]
+
+            print('Configure parameters successfully!')
 
         submit_button.on_clicked(submit_callback)
 
@@ -204,73 +268,65 @@ class InteractiveFHGenerator:
 
     def exec_generator(self):
         try:
-            self._fh_generator.generate_foghaze_images()
+            self.generation_result = self._fh_generator.generate_foghaze_images()[0]
+            messagebox.showinfo('Execute Generator', 'Done')
         except Exception as e:
+            traceback.print_exc()
             messagebox.showerror('Error', str(e))
-        messagebox.showinfo('Execute Generator', 'Done')
 
     
-    def show_clear(self):
+    def show_generation_result(self):
         try:
             clear = self._fh_generator.rgb_images[0]
-            plt.figure(5, clear=True)
-            plt.imshow(clear)
+            opmode = self._fh_generator.operation_mode
+            fh, idmap, real_atm_light, real_beta = self.generation_result
+
+            if type(real_atm_light) is np.ndarray:
+                real_atm_light = np.mean(real_atm_light)
+            if type(real_beta) is np.ndarray:
+                real_beta = np.mean(real_beta)
+
+            fig, axs = plt.subplots(2, 2, num=4, clear=True)
+            axs[0][0].set_title(f'Input Image {clear.shape}')
+            axs[0][0].imshow(clear)
+
+            axs[0][1].set_title(f'Foggy-Hazy Image (A = {real_atm_light}, β = {real_beta})')
+            axs[0][1].imshow(fh)
+
+            axs[1][0].set_title(f'Inverse Depth Map')
+            axs[1][0].imshow(idmap)
+
+            if opmode['scattering_coef'] == 'pnoise':
+                axs[1][1].set_title('Perlin Noise of β')
+                axs[1][1].imshow(self.perlin_noise_image())
+
             plt.show()
         except:
-            messagebox.showerror('Error', 'No clear image to display!')
-
+            traceback.print_exc()
+            messagebox.showerror('Error', 'No available generation result! Please execute generator first!')
     
-    def show_fh(self):
-        try:
-            fh = self._fh_generator.fh_images[0]
-            plt.figure(6, clear=True)
-            plt.imshow(fh)
-            plt.show()
-        except:
-            messagebox.showerror('Error', 'No foggy-hazy image to display!')
-
-
-    def show_idmap(self):
-        try:
-            idmap = self._fh_generator.inverse_dmaps[0]
-            plt.figure(7, clear=True)
-            plt.imshow(idmap)
-            plt.show()
-        except:
-            messagebox.showerror('Error', 'No inverse depth map to display!')
-
-
-    def show_pnoise_beta(self):
-        try:
-            plt.figure(8, clear=True)
-            print('Show Perlin noise')
-        except:
-            messagebox.showerror('Error', 'No Perlin noise of Beta to display!')
-
 
     def run(self):
         root = Tk()
         buttons = []
 
-        buttons.append(TkButton(root, text='Input RGB Image', command=self.input_rgb_image))
+        buttons.append(TkButton(root, text='Input RGB Image (*)', command=self.input_rgb_image))
+        buttons.append(TkButton(root, text='Input Gray Inverse Depth Map', command=self.input_inverse_dmap))
         buttons.append(TkButton(root, text='Configure Operation Mode', command=self.configure_opmode))
         buttons.append(TkButton(root, text='Configure Parameters', command=self.configure_params))
         buttons.append(TkButton(root, text='Execute Generator', command=self.exec_generator))
-        buttons.append(TkButton(root, text='Clear Image', command=self.show_clear))
-        buttons.append(TkButton(root, text='Foggy-Hazy Image', command=self.show_fh))
-        buttons.append(TkButton(root, text='Inverse Depth Map', command=self.show_idmap))
-        buttons.append(TkButton(root, text='Perlin Noise of Beta', command=self.show_pnoise_beta))
+        buttons.append(TkButton(root, text='Show Generation Result', command=self.show_generation_result))
 
         for i, btn in enumerate(buttons):
-            if i < 4:
+            if i < 3:
                 btn.grid(row=i, column=0, padx=20, pady=20)
             else:
-                btn.grid(row=i-4, column=1, padx=20, pady=20)
+                btn.grid(row=i-3, column=1, padx=20, pady=20)
 
         root.mainloop()
 
 
 if __name__ == '__main__':
     fh_generator = ASMFogHazeGenerator(MidasDmapEstimator())
-    experiment = InteractiveExperiment(fh_generator)
-    experiment.run()
+    interactive_program = InteractiveFHGenerator(fh_generator)
+    interactive_program.run()