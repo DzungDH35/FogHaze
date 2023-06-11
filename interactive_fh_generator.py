baf006821f1112e121446ed11ac4c2756eae75f1
Dzung DH <dzung.dh3510@gmail.com>
2023-06-11 22:58:48 +0700

    update logic to save idmap and foggy-hazy image

--- interactive_fh_generator.py
+++ interactive_fh_generator.py
@@ -10,122 +10,70 @@ from ast import literal_eval
 import cv2 as cv
 import traceback
 from foghaze_generator.helper import get_perlin_noise
+from datetime import datetime
 
 
-class Experiment2:
-    def print_img_info(self, print_img: bool = False):
-        img = self._rgb_image
-
-        print(
-            'Image information:',
-            f'(height, width, channel): {img.shape}',
-            f'Dtype: {img.dtype}',
-            f'Min value: {np.amin(img)}',
-            f'Max value: {np.amax(img)}',
-            sep='\n')
-        
-        if print_img:
-            print(img)
-        print()
-
-
-    def save_fh_image(self, fh_image: np.ndarray):
-        *dir_path, file_name = self.file_path
-        fh_name = f'{file_name.split(".")[0]}_fh.jpg'
-        fh_filepath = '/'.join(dir_path + [fh_name])
-
-        if os.path.exists(fh_filepath):
-            os.remove(fh_filepath)
-        plt.imsave(fh_filepath, fh_image)
-
-
-    def save_idmap(self, dmap: np.ndarray, cmap='gray'):
-        *dir_path, file_name = self.file_path
-        idmap_name = f'{file_name.split(".")[0]}_{cmap}.jpg'
-        idmap_filepath = '/'.join(dir_path + [idmap_name])
-
-        if os.path.exists(idmap_filepath):
-            os.remove(idmap_filepath)
-        plt.imsave(idmap_filepath, dmap, cmap=cmap)
-
+def print_img_info(img, print_img: bool = False):
+    print(
+        'Image information:',
+        f'(height, width, channel): {img.shape}',
+        f'Dtype: {img.dtype}',
+        f'Min value: {np.amin(img)}',
+        f'Max value: {np.amax(img)}',
+        sep='\n')
     
-    def _load_image_info(self):
-        idmap = self._fh_generator.inverse_dmaps
-
-
-    def plot_perlin_noise(self):
-        fig, axs = plt.subplots(1, 2)
-
-
-
-    """
-    pair: a tuple of (clear RGB image, foggy-hazy RGB image)
-    interactive_params: configurations for interactive params. Format is like:
-    {
-        'param_name': {
-            'current_val': int | float | np.ndarray - if np.ndarray, will display average
-            'bounds': tuple (low, high),
-            'step': int | float
-        }
-    }
-    fh_generator: the foghaze generator
-    """
-    def plot_interactive_pair(pair: tuple, interactive_params: dict, fh_generator: ASMFogHazeGenerator):
-        fig, axs = plt.subplots(1, 2)
-        axs[0].imshow(pair[0])
-        axs[0].set_title('Clear')
-
-        fh_axs = axs[1].imshow(pair[1])
-        axs[1].set_title('Foggy-Hazy')
-
-        sliders = {}
-        param_counts = len(interactive_params)
-        bottom_offset = 0.05 * (param_counts + 1) 
-        for key, config in interactive_params.items():
-            bottom_offset -= 0.05
-            valmin, valmax = config['bounds']
-
-            sliders[key] = Slider(
-                fig.add_axes([0.5, bottom_offset, 0.4, 0.05]),
-                label = key,
-                valmin = valmin, valmax = valmax,
-                valinit = config['current_val'], 
-                valstep = config['step']
-            )
-        
-        submit_btn = Button(fig.add_axes([0.8, 0.015, 0.1, 0.04]), 'Submit')
-        # text_box = plt.text(0, 0, 'Hello', transform=fig.add_axes([0.1, 0.4, 0.4, 0.1]).transAxes)
-
-        def _update_fh_image(event):
-            new_val = {}
-            for key, slider in sliders.items():
-                new_val[key] = slider.val
-            
-            fh_generator.atm_lights = [new_val['A']]
-            fh_generator.scattering_coefs = [new_val['Beta']]
-            new_fh = fh_generator.generate_foghaze_images()[0]
-
-            fh_axs.set_data(new_fh)
-            fig.canvas.draw_idle()
-            print('Done updating image.')
-
-        submit_btn.on_clicked(_update_fh_image)
-
-        if param_counts > 3:
-            fig.subplots_adjust(bottom=(0.15 * (param_counts - 3)))
-
-        plt.show()
-
-
+    if print_img:
+        print(img)
+    print()
 
 
 class InteractiveFHGenerator:
     _fh_generator: ASMFogHazeGenerator
+    file_path: str
     generation_result: tuple
 
 
     def __init__(self, fh_generator: ASMFogHazeGenerator):
         self._fh_generator = fh_generator
+
+
+    def get_time_suffix_filepath(self, file_path):
+        file_path = os.path.splitext(file_path)
+        file_path[0] += datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+        return ''.join(file_path)
+
+    
+    def save_fh_image(self):
+        try:
+            fh_image = self._fh_generator.fh_images[0]
+            *dir_path, file_name = self.file_path.split('/')
+            fh_name = f'{file_name.split(".")[0]}_fh.jpg'
+            fh_filepath = '/'.join(dir_path + [fh_name])
+
+            if os.path.exists(fh_filepath):
+                fh_filepath = self.get_time_suffix_filepath(fh_filepath)
+            plt.imsave(fh_filepath, fh_image)
+            messagebox.showinfo('Save Foggy-Hazy Image', f'Save image successfully at {fh_filepath}!')
+        except Exception as e:
+            traceback.print_exc()
+            messagebox.showerror('Error', str(e))
+
+
+    def save_idmap(self, cmap='gray'):
+        try:
+            idmap = self._fh_generator.inverse_dmaps[0]
+            *dir_path, file_name = self.file_path.split('/')
+            idmap_name = f'{file_name.split(".")[0]}_{cmap}.jpg'
+            idmap_filepath = '/'.join(dir_path + [idmap_name])
+
+            if os.path.exists(idmap_filepath):
+                idmap_filepath = self.get_time_suffix_filepath(idmap_filepath)
+            plt.imsave(idmap_filepath, idmap, cmap=cmap)
+            messagebox.showinfo('Save Inverse Depth Map', f'Save inverse depth map successfully at {idmap_filepath}!')
+        except Exception as e:
+            traceback.print_exc()
+            messagebox.showerror('Error', str(e))
 
     
     def perlin_noise_image(self):
@@ -145,6 +93,7 @@ class InteractiveFHGenerator:
 
         if file_path:
             try:
+                self.file_path = file_path
                 image = plt.imread(file_path)
                 self._fh_generator.rgb_images = [image]
                 plt.figure(1, clear=True)
@@ -361,15 +310,17 @@ class InteractiveFHGenerator:
         buttons.append(TkButton(root, text='Configure Operation Mode', command=self.configure_opmode))
         buttons.append(TkButton(root, text='Configure Parameters', command=self.configure_params))
         buttons.append(TkButton(root, text='Execute Generator', command=self.exec_generator))
+        buttons.append(TkButton(root, text='Save Foggy-Hazy Image', command=self.save_fh_image))
+        buttons.append(TkButton(root, text='Save Inverse Depth Map', command=self.save_idmap))
         buttons.append(TkButton(root, text='Show Clear & Foggy-Hazy Image', command=self.show_clear_and_fh))
         buttons.append(TkButton(root, text='Show Depth Map & Perlin Noise', command=self.show_idmap_and_pnoise))
         buttons.append(TkButton(root, text='Show Generation Result', command=self.show_overall_info))
 
         for i, btn in enumerate(buttons):
-            if i < 4:
+            if i < 5:
                 btn.grid(row=i, column=0, padx=20, pady=20)
             else:
-                btn.grid(row=i-4, column=1, padx=20, pady=20)
+                btn.grid(row=i-5, column=1, padx=20, pady=20)
 
         root.mainloop()
 
