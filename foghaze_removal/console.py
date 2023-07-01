b014cc6a45aa69701f1b8c5df58546ef7e797df4
Dzung DH <dzung.dh3510@gmail.com>
2023-07-01 10:38:10 +0700

    change algo

--- foghaze_removal/console.py
+++ foghaze_removal/console.py
@@ -1,4 +1,4 @@
-from improved_dcp import DEFAULT_PATCH_SIZE, DEFAULT_OMEGA, DEFAULT_T0, DEFAULT_RADIUS, DEFAULT_EPS, defoghaze
+from dcp import DEFAULT_PATCH_SIZE, DEFAULT_OMEGA, DEFAULT_T0, DEFAULT_RADIUS, DEFAULT_EPS, defoghaze
 import argparse
 import cv2 as cv
 import math
