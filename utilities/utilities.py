d12ff9cb71165e14df6c938f0372ea73bb1585a0
Dzung DH <dzung.dh3510@gmail.com>
2023-07-08 09:22:06 +0700

    update read_images_from_path() to have ability to index images read

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -29,31 +29,42 @@ def minmax_normalize(arr: np.ndarray, current_range: tuple = None, new_range: tu
 
 
 """
-Path, which is of an image or a directory of images, is read into a list of RGB or BGR images.
-color_mode is BGR (default) or RGB
+Path, which is of an image or a directory of images, is read into a list or dict (if indexed) of color or gray images.
+color_mode is BGR (default) or RGB or gray.
+If index = True, a dict indexed with filename.ext will be returned, otherwise, a list returned.
+If a list is returned, it is the result of images read in alphabetical order.
 """
-def read_images_from_path(path: str, color_mode='BGR') -> list:
+def read_images_from_path(path: str, color_mode='BGR', index: bool = True) -> dict|list:
+    file_names = []
     images = []
+    cv_imread_mode = cv.IMREAD_GRAYSCALE if color_mode == 'gray' else cv.IMREAD_COLOR
 
     if os.path.isfile(path) and os.path.splitext(path)[1].lower() in VALID_IMG_EXTENSIONS:
-        img = cv.imread(path)
+        img = cv.imread(path, cv_imread_mode)
 
         if img is None:
             raise Exception(f'Image path "{path}" cannot be read!')
+        
+        file_names.append(path)
         images.append(cv.imread(path))
     elif os.path.isdir(path):
         for file_name in sorted(os.listdir(path)):
 
             if os.path.splitext(file_name)[1].lower() in VALID_IMG_EXTENSIONS:
-                img = cv.imread(os.path.join(path, file_name))
+                img = cv.imread(os.path.join(path, file_name), cv_imread_mode)
 
                 if img is None:
                     raise Exception(f'Image path "{path}" cannot be read!')
+                
+                file_names.append(file_name)
                 images.append(img)
     else:
         raise Exception(f'Path "{path}" is not found in file system!')
-    
+
     if color_mode == 'RGB':
         images = [cv.cvtColor(img, cv.COLOR_BGR2RGB) for img in images]
+
+    if index is True:
+        return {index: content for index, content in zip(file_names, images)}
 
     return images
