d9f70296ec7ee994812a3c4aa405d7d6e09d5396
Dzung DH <dzung.dh3510@gmail.com>
2023-08-04 06:34:52 +0700

    add scripts to evaluate algorithm

--- scripts/msialdcp_0.9.sh
+++ scripts/msialdcp_0.9.sh
@@ -14,14 +14,14 @@ echo 'Done on O-HAZE'
 # DENSE-HAZE
 echo 'Start executing on DENSE-HAZE...'
 base='data-sets/DENSE-HAZE'
-python $defoghaze_path $base/'hazy_1' -gp=$base/'GT_1' -op=$base/'results/1' -dm=0 -ps=30 -fw=0.1 -arf=0.1 -pp=1 <<< $algo
-python $defoghaze_path $base/'hazy_2' -gp=$base/'GT_2' -op=$base/'results/2' -dm=0 -ps=30 -fw=0.1 -arf=0.1 -pp=1 <<< $algo
+python $defoghaze_path $base/'hazy_1' -gp=$base/'GT_1' -op=$base/'results/1' -dm=0 -ps=30 -fw=0.9 -arf=0.1 -pp=1 <<< $algo
+python $defoghaze_path $base/'hazy_2' -gp=$base/'GT_2' -op=$base/'results/2' -dm=0 -ps=30 -fw=0.9 -arf=0.1 -pp=1 <<< $algo
 wait
 echo 'Done on DENSE-HAZE'
 
 # NH-HAZE
 echo 'Start executing on NH-HAZE...'
 base='data-sets/NH-HAZE'
-python $defoghaze_path $base/'hazy_1' -gp=$base/'GT_1' -op=$base/'results/1' -dm=0 -ps=30 -fw=0.5 -arf=0.1 -pp=1 <<< $algo
-python $defoghaze_path $base/'hazy_2' -gp=$base/'GT_2' -op=$base/'results/2' -dm=0 -ps=30 -fw=0.5 -arf=0.1 -pp=1 <<< $algo
+python $defoghaze_path $base/'hazy_1' -gp=$base/'GT_1' -op=$base/'results/1' -dm=0 -ps=30 -fw=0.9 -arf=0.1 -pp=1 <<< $algo
+python $defoghaze_path $base/'hazy_2' -gp=$base/'GT_2' -op=$base/'results/2' -dm=0 -ps=30 -fw=0.9 -arf=0.1 -pp=1 <<< $algo
 echo 'Done on NH-HAZE'
