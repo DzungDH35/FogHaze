d9c636d28ab20a3af82f43e6cece75e0fba1b49e
Dzung DH <dzung.dh3510@gmail.com>
2023-08-10 08:20:23 +0700

    remove commented code

--- app/main.py
+++ app/main.py
@@ -13,45 +13,6 @@ import uvicorn
 app = FastAPI()
 
 
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-
-
-# @app.post("/upscale")
-# async def upload(
-#         file: UploadFile = File(...),
-#         scale: bool = Form(),
-#         denoise: bool = Form(),
-#         must_defog: bool = Form()
-# ):
-#     try:
-#         contents = await file.read()
-#         image = cv.imdecode(np.asarray(bytearray(contents), dtype="uint8"), cv.IMREAD_COLOR)
-#         print(scale, denoise, must_defog)
-#         if scale:
-#             if image.shape[0] > 1500 or image.shape[1] > 1500:
-#                 return JSONResponse(
-#                     status_code=status.HTTP_400_BAD_REQUEST,
-#                     content={"mgs": "file too large to scale, pls choose size < 1500x1500"}
-#                 )
-
-#             image = sr.upscale_image(image)
-#         if must_defog:
-#             image = defog(image, 3)
-#         if denoise:
-#             image = cv.fastNlMeansDenoisingColored(image, None, 3, 10, 7, 21)
-
-#         filename, file_extension = os.path.splitext(file.filename)
-
-#         _, bts = cv.imencode(file_extension, image)
-
-#         return StreamingResponse(io.BytesIO(bts.tobytes()), media_type=file.content_type)
-
-#     except Exception as e:
-#         print(e)
-#         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"mgs": e})
-
 def white_balance(image):
     image_float = image.astype(np.float32) / 255.0
 
