d48882c0ff5eb05e910d22fd387fbdc3c8664987
DzungDH <dungdh@bsscommerce.com>
2024-11-11 11:19:43 +0700

    Add script to preprocess dataset

---
+++ datasets/preprocess_NH-Haze.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+: '
+This file is used to partition files into GT and hazy folder pairs, 
+each folder contains up to FILE_BATCH_SIZE files (for example FILE_BATCH_SIZE = 20).
+'
+
+readonly DATASET_DIR="./NH-HAZE"
+readonly FILE_BATCH_SIZE=20
+cd "$DATASET_DIR" || exit 1
+
+echo "Start processing $DATASET_DIR..."
+
+hazy_files=()
+gt_files=()
+
+for file in *; do
+    if [[ -f "$file" ]]; then
+        if [[ "$file" == *hazy* ]]; then
+            hazy_files+=("$file")
+        elif [[ "$file" == *GT* ]]; then
+            gt_files+=("$file")
+        fi
+    fi
+done
+
+[ ${#hazy_files[@]} -ne ${#gt_files[@]} ] && { echo "Error: The number of hazy files does not match the number of GT files."; exit 1; }
+num_file_pairs=${#hazy_files[@]}
+
+create_dir_pairs() {
+    num_dirs=$(( ($num_file_pairs + $FILE_BATCH_SIZE - 1) / $FILE_BATCH_SIZE ))
+
+    for ((i=1; i<=num_dirs; ++i)); do
+        mkdir "GT_$i"
+        mkdir "hazy_$i"
+    done
+}
+
+# Partition hazy and GT files into pair of directories
+partition_files() {
+    dir_index=1
+
+    for ((i=0; i<num_file_pairs; ++i)); do
+        mv "${gt_files[$i]}" "./GT_$dir_index/$((i+1)).${gt_files[$i]##*.}"
+        mv "${hazy_files[$i]}" "./hazy_$dir_index/$((i+1)).${hazy_files[$i]##*.}"
+
+        (( (i+1) % FILE_BATCH_SIZE == 0 )) && ((dir_index++))
+    done
+}
+
+create_dir_pairs
+partition_files
+
+echo "The dataset is processed succesfully!"
--- scripts/msialdcp_0.9.sh
+++ scripts/msialdcp_0.9.sh
@@ -5,7 +5,7 @@ defoghaze_path='defoghaze.py'
 
 # O-HAZE
 echo 'Start executing on O-HAZE...'
-base='data-sets/O-HAZE'
+base='datasets/O-HAZE'
 python $defoghaze_path $base/'hazy_1' -gp=$base/'GT_1' -op=$base/'results/1' -dm=0 -ps=30 -fw=0.9 -arf=0.05 -pp=1 <<< $algo
 python $defoghaze_path $base/'hazy_2' -gp=$base/'GT_2' -op=$base/'results/2' -dm=0 -ps=30 -fw=0.9 -arf=0.05 -pp=1 <<< $algo
 python $defoghaze_path $base/'hazy_3' -gp=$base/'GT_3' -op=$base/'results/3' -dm=0 -ps=30 -fw=0.9 -arf=0.05 -pp=1 <<< $algo
@@ -13,7 +13,7 @@ echo 'Done on O-HAZE'
 
 # DENSE-HAZE
 echo 'Start executing on DENSE-HAZE...'
-base='data-sets/DENSE-HAZE'
+base='datasets/DENSE-HAZE'
 python $defoghaze_path $base/'hazy_1' -gp=$base/'GT_1' -op=$base/'results/1' -dm=0 -ps=30 -fw=0.9 -arf=0.1 -pp=1 <<< $algo
 python $defoghaze_path $base/'hazy_2' -gp=$base/'GT_2' -op=$base/'results/2' -dm=0 -ps=30 -fw=0.9 -arf=0.1 -pp=1 <<< $algo
 wait
@@ -21,7 +21,7 @@ echo 'Done on DENSE-HAZE'
 
 # NH-HAZE
 echo 'Start executing on NH-HAZE...'
-base='data-sets/NH-HAZE'
+base='datasets/NH-HAZE'
 python $defoghaze_path $base/'hazy_1' -gp=$base/'GT_1' -op=$base/'results/1' -dm=0 -ps=30 -fw=0.9 -arf=0.1 -pp=1 <<< $algo
 python $defoghaze_path $base/'hazy_2' -gp=$base/'GT_2' -op=$base/'results/2' -dm=0 -ps=30 -fw=0.9 -arf=0.1 -pp=1 <<< $algo
 echo 'Done on NH-HAZE'
